7d6724e2227beedec04349468f3f2b5fda07da61
Lynn Nguyen <nplinh@gmail.com>
2024-10-08 21:36:05 +1100

    Modify outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "name" { 
     description = "The name of the key vault created"
-    value       = azurerm_key_vault.this.name
+    value       = azurerm_key_vault.example.name
 }
 
 output "id"  {
     description = "The id of the key vault created"
-    value       = azurerm_key_vault.this.value
+    value       = azurerm_key_vault.example.id
 }
